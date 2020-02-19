7f1bf87899c516c604e50bcefab3c81b38c6e196
unmadebed <kevin.archer@live.com>
2020-02-19 16:33:01 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Countries</h1>
+    <countries-list :countries='countries'></countries-list>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CountriesList from './components/CountriesList.vue'
+import { eventBus } from './main.js'
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      countries: [],
+      selectedCountry: null
+    }
+
+  },
+  mounted() {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(result => result.json())
+    .then(countries => this.countries = countries)
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country;
+    })
+  },
+
   components: {
-    HelloWorld
+    "countries-list": CountriesList
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
