4d2e23d54709f5d7637386c6730b35d2c5815883
unmadebed <kevin.archer@live.com>
2020-02-19 23:18:02 +0000

    MVP

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template lang="html">
   <div>
     <h1>Countries</h1>
+    <div class="main-container">    
     <countries-list :countries='countries'></countries-list>
+    <country-detail :country='selectedCountry'></country-detail>
+    </div>
   </div>
 
 </template>
 
 <script>
-import CountriesList from './components/CountriesList.vue'
+import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
 import { eventBus } from './main.js'
+
 export default {
   name: 'app',
   data() {
@@ -29,11 +34,16 @@ export default {
   },
 
   components: {
-    "countries-list": CountriesList
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
 
   }
 }
 </script>
 
-<style lang="css" scoped>
+<style>
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
